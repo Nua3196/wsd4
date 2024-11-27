cd5d6e71563146cf85162fe1a5d28d3fbbc68d18
Nua3196 <mytale3@gmail.com>
2024-11-27 19:18:18 +0900

    찜하기 기능 추가

--- src/components/MoviePoster.vue
+++ src/components/MoviePoster.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="movie-card" @mouseover="hover = true" @mouseleave="hover = false">
+  <div
+    class="movie-card"
+    @mouseover="hover = true"
+    @mouseleave="hover = false"
+    @click="toggleWishlist"
+  >
     <img :src="movie.backdrop_path" :alt="movie.title" class="poster" />
     <!-- 마우스 호버 시 추가 정보 표시 -->
     <div class="info" v-if="hover">
@@ -12,17 +17,21 @@
         </span>
       </div> -->
     </div>
+    <!-- 찜 여부 표시 -->
+    <div class="wishlist-indicator" v-if="isInWishlist">❤️</div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, computed } from "vue";
+import { useStore } from "vuex";
 
 export default defineComponent({
   name: "MoviePoster",
   props: {
     movie: {
       type: Object as () => {
+        id: number;
         title: string;
         backdrop_path: string;
         overview: string;
@@ -33,7 +42,22 @@ export default defineComponent({
     },
   },
   setup(props) {
+    const store = useStore(); // Vuex Store 사용
     const hover = ref(false); // 호버 상태 관리
+
+    // Wishlist에 있는지 확인
+    const isInWishlist = computed(() =>
+      store.getters.getWishlist.includes(props.movie.id)
+    );
+
+    // Wishlist 추가/제거 토글
+    const toggleWishlist = () => {
+      if (isInWishlist.value) {
+        store.dispatch("removeMovieFromWishlist", props.movie.id);
+      } else {
+        store.dispatch("addMovieToWishlist", props.movie.id);
+      }
+    };
 
     // 영화 설명을 일정 길이까지만 표시하고 초과 시 ... 처리
     const maxLength = 30; // 최대 표시 길이
@@ -43,7 +67,7 @@ export default defineComponent({
         : props.movie.overview;
     });
 
-    return { hover, truncatedOverview };
+    return { hover, truncatedOverview, isInWishlist, toggleWishlist };
   },
 });
 </script>
@@ -62,6 +86,21 @@ export default defineComponent({
 .movie-card:hover {
   transform: scale(1.1); /* 확대 효과 */
   box-shadow: 0 8px 15px rgba(0, 0, 0, 0.3); /* 그림자 효과 */
+}
+
+/* 찜 상태를 나타내는 하트 아이콘 스타일 */
+.wishlist-indicator {
+  position: absolute; /* 부모 요소(movie-card) 기준으로 배치 */
+  top: 5px; /* 카드의 상단에서 10px 아래 */
+  right: 5px; /* 카드의 오른쪽에서 10px 안쪽 */
+  font-size: 1.7rem; /* 적당한 크기의 하트 아이콘 */
+  z-index: 10; /* 다른 요소 위에 표시되도록 우선순위 설정 */
+  cursor: pointer;
+}
+
+/* 호버 시 하트 강조 */
+.wishlist-indicator:hover {
+  top: 1px;
 }
 
 .poster {
