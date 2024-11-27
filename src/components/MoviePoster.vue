17145af43d42c76dd91c9e3f1c7056752a1d6feb
Nua3196 <mytale3@gmail.com>
2024-11-27 13:15:26 +0900

    무비 틀 생성

--- src/components/MoviePoster.vue
+++ src/components/MoviePoster.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="movie-poster">
-    <!-- Movie Poster content will go here -->
+    <!-- 영화 포스터 -->
+    <img :src="movie.posterPath" :alt="movie.title" />
+
+    <!-- 영화 제목 -->
+    <div class="title">{{ movie.title }}</div>
+
+    <!-- 영화 설명 -->
+    <div class="description">{{ movie.description }}</div>
+
+    <!-- 영화 개봉일 -->
+    <div class="release-date">Release Date: {{ movie.releaseDate }}</div>
+
+    <!-- 영화 장르 -->
+    <div class="genres">
+      Genres:
+      <span v-for="(genre, index) in movie.genres" :key="index" class="genre">
+        {{ genre }}
+      </span>
+    </div>
   </div>
 </template>
 
@@ -11,7 +29,13 @@ export default defineComponent({
   name: "MoviePoster",
   props: {
     movie: {
-      type: Object as PropType<{ title: string; posterPath: string }>,
+      type: Object as PropType<{
+        title: string;
+        posterPath: string;
+        description: string;
+        releaseDate: string;
+        genres: string[];
+      }>,
       required: true,
     },
   },
@@ -21,13 +45,44 @@ export default defineComponent({
 <style lang="scss">
 .movie-poster {
   width: 200px;
+  border: 1px solid #ddd; // 외곽선 추가
+  border-radius: 10px;
+  padding: 10px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  background-color: #fff;
+
   img {
     width: 100%;
     border-radius: 10px;
+    margin-bottom: 10px;
   }
+
   .title {
     font-weight: bold;
-    margin-top: 10px;
+    font-size: 1.2em;
+    margin-bottom: 10px;
+  }
+
+  .description {
+    font-size: 0.9em;
+    color: #666;
+    margin-bottom: 10px;
+  }
+
+  .release-date {
+    font-size: 0.8em;
+    margin-bottom: 10px;
+    color: #333;
+  }
+
+  .genres {
+    font-size: 0.8em;
+    .genre {
+      margin-right: 5px;
+      padding: 2px 5px;
+      background-color: #f1f1f1;
+      border-radius: 5px;
+    }
   }
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,53 @@
 <template>
   <div class="home">
-    <!-- Home page content will go here -->
+    <div class="movies">
+      <!-- MoviePoster 컴포넌트를 동적으로 렌더링 -->
+      <MoviePoster
+        v-for="(movie, index) in movies"
+        :key="index"
+        :movie="movie"
+      />
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import MoviePoster from "@/components/MoviePoster.vue";
 
 export default defineComponent({
   name: "HomeView",
+  components: {
+    MoviePoster,
+  },
+  data() {
+    return {
+      // 여러 영화 정보를 관리하는 데이터
+      movies: [
+        {
+          title: "Inception",
+          posterPath: "https://image.tmdb.org/t/p/w200/xyz.jpg",
+          description: "A mind-bending thriller about dream invasion.",
+          releaseDate: "2010-07-16",
+          genres: ["Action", "Sci-Fi", "Thriller"],
+        },
+        {
+          title: "The Dark Knight",
+          posterPath: "https://image.tmdb.org/t/p/w200/abc.jpg",
+          description: "A vigilante must confront chaos in Gotham.",
+          releaseDate: "2008-07-18",
+          genres: ["Action", "Drama", "Crime"],
+        },
+        {
+          title: "Interstellar",
+          posterPath: "https://image.tmdb.org/t/p/w200/def.jpg",
+          description: "Exploration of space and love across dimensions.",
+          releaseDate: "2014-11-07",
+          genres: ["Adventure", "Drama", "Sci-Fi"],
+        },
+      ],
+    };
+  },
 });
 </script>
 
