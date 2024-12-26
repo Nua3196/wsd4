f53fdd74df1e26637a9a7dad7d8db4d162b2864e
Sakang Hone <mytale3@gmail.com>
2024-12-26 18:01:24 +0900

    trying...

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,4 @@
 import ESLintPlugin from 'eslint-webpack-plugin';
-import webpack from 'webpack';
 
 export default {
     lintOnSave: false,
@@ -9,16 +8,6 @@ export default {
             failOnError: false,
             emitWarning: true,
         }),
-        new webpack.DefinePlugin({
-            'process.env': {
-                VUE_APP_IP_ADDRESS: JSON.stringify(process.env.VUE_APP_IP_ADDRESS),
-                VUE_APP_KAKAO_API_KEY: JSON.stringify(process.env.VUE_APP_KAKAO_API_KEY),
-                VUE_APP_LOG_LEVEL: JSON.stringify(process.env.VUE_APP_LOG_LEVEL),
-                VUE_APP_PORT: JSON.stringify(process.env.VUE_APP_PORT),
-                VUE_APP_SCHEME: JSON.stringify(process.env.VUE_APP_SCHEME),
-                VUE_APP_TMDB_API_KEY: JSON.stringify(process.env.VUE_APP_TMDB_API_KEY)
-            }
-        })
         ],
     },
 };
