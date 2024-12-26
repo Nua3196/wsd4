6898b9e49cc855c26534e2add539bf533018721b
Sakang Hone <mytale3@gmail.com>
2024-12-26 17:50:39 +0900

    trying...

--- vue.config.js
+++ vue.config.js
@@ -10,4 +10,14 @@ export default {
         }),
         ],
     },
+    chainWebpack: config => {
+        config.plugin('define').tap(args => {
+            Object.keys(process.env).forEach(key => {
+                if (key.startsWith('VUE_APP_')) {
+                    args[0][`process.env.${key}`] = JSON.stringify(process.env[key])
+                }
+            })
+            return args
+        })
+    }
 };
